762a9d25d3db4c656c1f3f3cdae13b456b973aa9
Pmz94 <pmz94@hotmail.com>
2018-07-02 15:59:49 -0700

    moviendo mas jQuery

--- pagpersonal/asset/js/script.js
+++ pagpersonal/asset/js/script.js
@@ -3,7 +3,7 @@ $(function() {
 	$('.navbar-burger').click(function() {
 		$('#navRight, .navbar-burger').toggleClass('is-active');
 	});
-	$('img').attr('draggable', 'false');
+	$('img').attr('draggable', false);
 	$('img').bind('contextmenu', function() {
 		return false;
 	});
