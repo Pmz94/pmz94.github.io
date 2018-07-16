0ba710cc0d288822faec88b24d436ae6c070c5f5
Pmz94 <pmz94@hotmail.com>
2018-07-16 15:46:04 -0700

    .

---
+++ pagpersonal/asset/js/trabajos.js
@@ -0,0 +1,92 @@
+var trabajo = [
+	{
+		'titulo': 'Impartición de asesorías de Probabilidad y Estadística',
+		'descripcion': 'Contenido de estadística descriptiva, probabilidad y estadística inferencial. Hobby personal.',
+		'tiempo': '2015-2 - 2018-1',
+		'icono1': 'dados.png',
+		'icono2': 'distribucionnormal.png',
+		'altico1': '*Dados*',
+		'altico2': '*Distribucion Normal*',
+		'class': 'a'
+	},
+	{
+		'titulo': 'Accidentes de tránsito en México de 1997 a 2015',
+		'descripcion': 'Análisis estadístico de base de datos con objetivo de hallar patrones, indicios, predecir probabilidades y dar resultados. Trabajo Personal.',
+		'tiempo': '2017-2',
+		'icono1': 'choque.png',
+		'icono2': 'analisis.png',
+		'altico1': '*Choque*',
+		'altico2': '*Analisis*',
+		'class': 'b'
+	},
+	{
+		'titulo': 'Página web personal',
+		'descripcion': 'Un pequeño espacio donde soy yo mismo y pueden saber más de mí. Desarrollada en HTML con el framework Bulma en CSS. Trabajo Personal.',
+		'tiempo': '2017-2',
+		'icono1': 'website.png',
+		'icono2': 'llaveshtml.png',
+		'altico1': '*Sitio Web*',
+		'altico2': '*LLaves de HTML*',
+		'class': 'a'
+	},
+	{
+		'titulo': 'Capacidad hedónica como mecanismo protector contra el estrés en alumnos universitarios',
+		'descripcion': 'Trabajo de investigación aprobado por PRODEP. Universidad de Sonora.',
+		'tiempo': 'Agosto 2017 - Diciembre 2017',
+		'icono1': 'estres.png',
+		'icono2': 'tarea.png',
+		'altico1': '*Persona estresada*',
+		'altico2': '*Tarea*',
+		'class': 'b'
+	},
+	{
+		'titulo': 'Capacidad de revaloración cognitiva en la protección contra el estrés y los síntomas psiquiátricos: posibles mecanismos biológicos',
+		'descripcion': 'Trabajo de investigación pre-aprobado por CONACYT. Universidad de Sonora.',
+		'tiempo': 'Junio de 2017 - Diciembre 2017',
+		'icono1': 'cerebro.png',
+		'icono2': 'cuchillo.png',
+		'altico1': '*Cerebro*',
+		'altico2': '*Cuchillo*',
+		'class': 'a'
+	},
+	{
+		'titulo': 'Implementación de Plan Estratégico Informático a Cocina Económica “Sabor a Mi”',
+		'descripcion': 'Integración de TI’s a los procesos de la empresa con fines de mejora a ésta. Proyecto de Planeación Estratégica Informática.',
+		'tiempo': '2017-1',
+		'icono1': 'lista.png',
+		'icono2': 'estrategia.png',
+		'altico1': '*Lista*',
+		'altico2': '*Estrategia*',
+		'class': 'b'
+	},
+	{
+		'titulo': 'Desarrollo de sistema de control ganadero “MooPlot”',
+		'descripcion': 'Sistema de información de control de ganado. Desarrollo de la base de datos del sistema. Desarrollado en ASP.NET MVC 5 en Visual Studio.',
+		'tiempo': '2016-2',
+		'icono1': 'mooplot.png',
+		'icono2': 'granja.png',
+		'altico1': '*MooPlot*',
+		'altico2': '*Granja*',
+		'class': 'a'
+	},
+	{
+		'titulo': 'Proyecto "Manos a la Obra"',
+		'descripcion': 'Servicio social sin ánimos de lucro dedicado a la donación de ropa a casa hogar "Ciudad de los Niños".',
+		'tiempo': '2015-1',
+		'icono1': 'manosalaobra.png',
+		'icono2': 'calcetin.png',
+		'altico1': '*Manos a la Obra*',
+		'altico2': '*Calcetin*',
+		'class': 'b'
+	},
+	{
+		'titulo': 'Plataforma de Mini Juegos',
+		'descripcion': 'Aplicación de Pares, Buscaminas y un Test. Desarrollada en Java en NetBeans. Trabajo personal.',
+		'tiempo': '2015-1',
+		'icono1': 'cartas.png',
+		'icono2': 'mina.png',
+		'altico1': '*Cartas*',
+		'altico2': '*Mina*',
+		'class': 'a'
+	}
+];
